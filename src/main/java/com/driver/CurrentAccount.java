422d7cccc17eaff78e407b5b07c3b066da76315c
Gauatm-raj <gautamraj7569@gmail.com>
2024-01-13 14:08:08 +0530

    Assignment Done

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,11 +1,19 @@
 package com.driver;
 
+
+import java.util.HashMap;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
-        // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name,balance,5000.00);
+       this.tradeLicenseId=tradeLicenseId;
 
+        // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        if (balance < 5000) {
+            throw new Exception("Insufficient Balance. Minimum balance required is 5000.");
+        }
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,7 +21,33 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        for (int i = 0; i < tradeLicenseId.length() - 1; i++) {
+            if (tradeLicenseId.charAt(i) == tradeLicenseId.charAt(i + 1)) {
+                throw new Exception("Valid License can not be generated");
+            }
+        }
+        if(!canValid()){
+            throw new Exception("Valid License can not be generated");
+        }
+    }
+    public boolean canValid(){
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        for (char c : tradeLicenseId.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
 
+        // Check if all character counts are even (allowing at most one character with an odd count)
+        int oddCount = 0;
+        for (int count : charCount.values()) {
+            if (count % 2 != 0) {
+                oddCount++;
+                if (oddCount > 1) {
+                    return false; // More than one character with an odd count
+                }
+            }
+        }
+
+        return true;
     }
 
 }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,14 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+      SavingsAccount s1=new SavingsAccount("Gautam Raj",1000.00,500.00,10.0);
+      CurrentAccount c1=new CurrentAccount("Jannat Raj",10500.00,"TRADE");
+      StudentAccount student=new StudentAccount("Ram",4500.00,"University");
+
+      String studentAccountNO=s1.generateAccountNumber(54321,15);
+      student.deposit(500.00);
+      student.withdraw(100.00);
 
     }
 }
